61a37f76f133d3a5ed374704fb11e4bd03d162b9
HTakuto <htakutolife@gmail.com>
2023-10-19 21:15:57 +0900

    タスク更新ルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,3 +19,4 @@ Route::get('/{id}', [TasksController::class, 'show'])->name('tasks.show');
 Route::get('/tasks/add', [TasksController::class, 'add'])->name('tasks.add');
 Route::post('/tasks/add', [TasksController::class, 'store'])->name('tasks.store');
 Route::get('/tasks/edit/{id}', [TasksController::class, 'edit'])->name('tasks.edit');
+Route::post('tasks/edit/{id}', [TasksController::class, 'update'])->name('tasks.update');
