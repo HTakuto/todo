96887ed0378f9c31447f1506ad520129cbe0858a
HTakuto <htakutolife@gmail.com>
2023-10-19 17:08:46 +0900

    タスク詳細コントローラー

--- src/app/Http/Controllers/TasksController.php
+++ src/app/Http/Controllers/TasksController.php
@@ -12,4 +12,10 @@ class TasksController extends Controller
         $tasks = Task::get();
         return view('tasks.index', compact('tasks'));
     }
+
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return view('tasks.show', compact('task'));
+    }
 }
